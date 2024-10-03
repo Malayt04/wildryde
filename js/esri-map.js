52ccead1d7b38e315e978e4a16256f978d519e3b
Malay Tiwari <malayt04@gmail.com>
2024-10-03 09:39:07 +0530

    .

--- js/esri-map.js
+++ js/esri-map.js
@@ -23,7 +23,6 @@ WildRydes.map = WildRydes.map || {};
         var map = new Map({ basemap: 'gray-vector' });
 
         var view = new MapView({
-            center: [-122.31, 47.60],
             container: 'map',
             map: map,
             zoom: 12
