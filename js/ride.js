7f9e105b94ab3cfa88b404caa8b84edbcf404bff
Malay Tiwari <malayt04@gmail.com>
2024-10-07 16:41:31 +0530

    api

--- js/ride.js
+++ js/ride.js
@@ -5,6 +5,7 @@ WildRydes.map = WildRydes.map || {};
 
 (function rideScopeWrapper($) {
     var authToken;
+
     WildRydes.authToken.then(function setAuthToken(token) {
         if (token) {
             authToken = token;
@@ -15,6 +16,7 @@ WildRydes.map = WildRydes.map || {};
         alert(error);
         window.location.href = '/signin.html';
     });
+
     function requestUnicorn(pickupLocation) {
         $.ajax({
             method: 'POST',
@@ -33,27 +35,24 @@ WildRydes.map = WildRydes.map || {};
             error: function ajaxError(jqXHR, textStatus, errorThrown) {
                 console.error('Error requesting ride: ', textStatus, ', Details: ', errorThrown);
                 console.error('Response: ', jqXHR.responseText);
-                alert('An error occured when requesting your unicorn:\n' + jqXHR.responseText);
+                alert('An error occurred when requesting your unicorn:\n' + jqXHR.responseText);
             }
         });
     }
 
     function completeRequest(result) {
-        var unicorn;
-        var pronoun;
-        console.log('Response received from API: ', result);
-        unicorn = result.Unicorn;
-        pronoun = unicorn.Gender === 'Male' ? 'his' : 'her';
+        var unicorn = result.Unicorn;
+        var pronoun = unicorn.Gender === 'Male' ? 'his' : 'her';
         displayUpdate(unicorn.Name + ', your ' + unicorn.Color + ' unicorn, is on ' + pronoun + ' way.');
+        
         animateArrival(function animateCallback() {
             displayUpdate(unicorn.Name + ' has arrived. Giddy up!');
             WildRydes.map.unsetLocation();
-            $('#request').prop('disabled', 'disabled');
+            $('#request').prop('disabled', true);
             $('#request').text('Set Pickup');
         });
     }
 
-    // Register click handler for #request button
     $(function onDocReady() {
         $('#request').click(handleRequestClick);
         $('#signOut').click(function() {
@@ -61,6 +60,7 @@ WildRydes.map = WildRydes.map || {};
             alert("You have been signed out.");
             window.location = "signin.html";
         });
+        
         $(WildRydes.map).on('pickupChange', handlePickupChanged);
 
         WildRydes.authToken.then(function updateAuthMessage(token) {
@@ -91,17 +91,13 @@ WildRydes.map = WildRydes.map || {};
         var dest = WildRydes.map.selectedPoint;
         var origin = {};
 
-        if (dest.latitude > WildRydes.map.center.latitude) {
-            origin.latitude = WildRydes.map.extent.minLat;
-        } else {
-            origin.latitude = WildRydes.map.extent.maxLat;
-        }
+        origin.latitude = dest.latitude > WildRydes.map.center.latitude ? 
+                         WildRydes.map.extent.minLat : 
+                         WildRydes.map.extent.maxLat;
 
-        if (dest.longitude > WildRydes.map.center.longitude) {
-            origin.longitude = WildRydes.map.extent.minLng;
-        } else {
-            origin.longitude = WildRydes.map.extent.maxLng;
-        }
+        origin.longitude = dest.longitude > WildRydes.map.center.longitude ? 
+                          WildRydes.map.extent.minLng : 
+                          WildRydes.map.extent.maxLng;
 
         WildRydes.map.animate(origin, dest, callback);
     }
